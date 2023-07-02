b7e04bf21f7117ae921eccbb66c12974dac8096f
Ahmed Henawy <111087686+AhmedHenawy11@users.noreply.github.com>
2023-07-03 00:15:14 +0300

    Add files via upload

---
+++ Band name generator/main.py
@@ -0,0 +1,5 @@
+# This program contains the next topics[ Printing - Inputs - Variables - Concatination ]
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's you pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
